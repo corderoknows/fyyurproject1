d6ecf64a368ad65dc94563d31da95cc36a2a5639
Cordero W <129771033+corderoknows@users.noreply.github.com>
2024-01-25 07:30:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,6 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/fyyurproject1'
+# Remove console warning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
